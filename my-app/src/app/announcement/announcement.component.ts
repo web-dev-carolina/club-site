8b9d1de9c264a1abd9f4cee54d686ee711f45b88
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-10-07 18:10:06 -0400

    Removed mod id

--- my-app/src/app/announcement/announcement.component.ts
+++ my-app/src/app/announcement/announcement.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'app-announcement',
   templateUrl: 'announcement.component.html',
   styleUrls: ['announcement.component.css'],
