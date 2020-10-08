289c85835f7d20f6a8d9ba4d794ab94ef145378a
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-10-08 17:07:10 -0400

    Added contact-us component and fixed routing

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ProjectComponent } from './project/project.component';
 import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
@@ -20,8 +22,13 @@ import { TestimonialComponent } from './testimonial/testimonial.component';
     AboutUsComponent,
     ProjectComponent,
     TestimonialComponent,
+    ContactUsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([{ path: '', component: HomeComponent }]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
