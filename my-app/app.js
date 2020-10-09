6a3633148c303b6fd4adcaaf6c52d4070efc87d1
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-10-08 21:26:26 -0400

    Configure express

---
+++ my-app/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+app.get("*", (req, res) => {
+  var options = {
+    root: path.join(__dirname, "dist/club-site"),
+  };
+
+  return res.sendFile("index.html", options);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
