337ad5e896d111958884a5d5c50928ead9c810c2
Advaith Deo <advaith.deo@gmail.com>
2020-10-10 16:10:24 -0400

    adding home to app

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,7 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <!-- header -->
+  <app-header></app-header>
+
+  <!-- testing the home page -->
+  <app-home></app-home>
+
+  <!-- routes will be rendered here -->
+  <router-outlet></router-outlet>
+
+  <!-- footer -->
+  <app-footer></app-footer>
+`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
