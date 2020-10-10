9761c84e6ebb29208ca8dd504c3a880fa1a58437
Advaith Deo <advaith.deo@gmail.com>
2020-10-10 16:06:45 -0400

    home updates

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'app-home',
-  templateUrl: 'home.component.html',
-  styleUrls: ['home.component.css'],
+  template: `
+    <section class="hero is-info is-fullheight is-bold">
+    <div class="hero-body">
+    <div class="container">
+
+      <h1 class="title">Home Page!</h1>
+
+    </div>
+    </div>
+    </section>
+  `,
+  styles: []
 })
 export class HomeComponent {}
