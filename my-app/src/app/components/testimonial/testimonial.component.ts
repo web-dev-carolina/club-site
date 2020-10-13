bee8d639f88dc3dddb5021a830930917dce22b0e
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-10-13 14:59:53 -0400

    Fix

--- my-app/src/app/components/testimonial/testimonial.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-testimonial',
-  templateUrl: 'testimonial.component.html',
-  styleUrls: ['testimonial.component.css'],
-})
-export class TestimonialComponent {}
