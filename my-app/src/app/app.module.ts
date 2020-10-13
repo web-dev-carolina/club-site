8c00696b248cb5a5c6f6bd994ee93c3cb35dd50a
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-10-13 15:14:10 -0400

    Fix

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ProjectComponent } from './components/project/project.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    AnnouncementComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ProjectComponent,
+    TestimonialComponent,
+    ContactUsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([{ path: '', component: HomeComponent }]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ my-app/src/app/components/announcement/announcement.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement',
+  templateUrl: 'announcement.component.html',
+  styleUrls: ['announcement.component.css'],
+})
+export class AnnouncementComponent {}
