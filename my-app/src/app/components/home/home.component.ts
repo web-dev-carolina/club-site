08f899998fe87542c8fe903a2d33ecb9a9226319
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-10-14 10:29:06 -0400

    Added more examples

--- my-app/src/app/components/home/home.component.ts
+++ my-app/src/app/components/home/home.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  
+}
