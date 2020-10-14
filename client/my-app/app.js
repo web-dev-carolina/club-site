05624db5cb0374c9fdc7d2cdcefbebe71f9d4140
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-10-14 18:20:30 -0400

    Update to Christian branch

--- client/my-app/app.js
+++ client/my-app/app.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({extended: false}));
+const path = require("path");
 
 app.get("*", (req, res) => {
   var options = {
