bd775447e73c58d6af58f9eaf0d71c0fb03a3fb2
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-10-28 08:41:35 -0400

    Remove old backend test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var router = express.Router()
+var mongojs = require('mongojs')
+var db = mongojs('mongodb+srv://cnell:David3284@cluster0.tak5v.mongodb.net/club-site?retryWrites=true&w=majority', ['tasks'])
 
 router.get('/', function(req, res, next){
     res.render('index.html')
--- server.js
+++ server.js
@@ -3,23 +3,13 @@ var path = require('path')
 var bodyParser = require('body-parser')
 
 var index = require('./routes/index')
-var tasks = require('./routes/tasks')
 
 var port = 3000;
 
 var app = express()
 
-//View Engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.engine('html', require('ejs').renderFile)
-
 //Set Static Folder
-//app.use(express.static(path.join(__dirname, 'client/my-app/src')))
 app.use('/', express.static('client/my-app/dist/my-app'))
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/my-app/dist/my-app'));
- });
 
 //Body Parser MW
 app.use(bodyParser.json())
@@ -27,8 +17,6 @@ app.use(bodyParser.urlencoded({extende: false}))
 
 //Home page
 app.use('/', index)
-//Interact with API
-app.use('/api', tasks)
 
 app.listen(port, function(){
     console.log("Server started on port" + port)
