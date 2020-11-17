4faaeeb6a0411702ea9c54bef2795595ba249bc9
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-17 16:08:46 -0500

    New password

--- routes/upcomingEvent.js
+++ routes/upcomingEvent.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs')
-var db = mongojs('mongodb+srv://cnell:r[~7vn&seP_DV&=t@cluster0.tak5v.mongodb.net/club-site?retryWrites=true&w=majority', ['upcomingEvents'])
+var db = mongojs('mongodb+srv://cnell:esYdTcq4kq364R4c@cluster0.tak5v.mongodb.net/club-site?retryWrites=true&w=majority', ['upcomingEvents'])
 
 router.get('/upcomingEvents', function(req, res, next){
     db.upcomingEvents.find(function(err, upcomingEvents){
