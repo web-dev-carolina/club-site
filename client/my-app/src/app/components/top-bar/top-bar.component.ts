aa97edeb0afad21f77fdf5dab029ed7dbd05ad03
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-03 18:36:10 -0500

    Change navbar back

--- client/my-app/src/app/components/top-bar/top-bar.component.ts
+++ client/my-app/src/app/components/top-bar/top-bar.component.ts
@@ -1,17 +1,5 @@
 import { Component } from '@angular/core';
 
-window.onscroll = () => {
-  const currentScrollPos = window.pageYOffset;
-
-  // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
-  if (currentScrollPos > 800) {
-    // I am using 'display' instead of 'top':
-    document.getElementById('white-nav').style.display = 'initial';
-  } else {
-    document.getElementById('white-nav').style.display = 'none';
-  }
-};
-
 @Component({
   selector: 'app-top-bar',
   templateUrl: 'top-bar.component.html',
