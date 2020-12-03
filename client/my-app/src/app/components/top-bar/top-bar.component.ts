cb1bd60fb4075c702c3afdead3b3611ce0b52631
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-02 19:29:49 -0500

    Add navbar display on scroll to home+

--- client/my-app/src/app/components/top-bar/top-bar.component.ts
+++ client/my-app/src/app/components/top-bar/top-bar.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
+window.onscroll = () => {
+  const currentScrollPos = window.pageYOffset;
+
+  // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
+  if (currentScrollPos > 1000) {
+    // I am using 'display' instead of 'top':
+    document.getElementById('white-nav').style.display = 'initial';
+  } else {
+    document.getElementById('white-nav').style.display = 'none';
+  }
+};
+
 @Component({
   selector: 'app-top-bar',
   templateUrl: 'top-bar.component.html',
   styleUrls: ['top-bar.component.css'],
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+}
