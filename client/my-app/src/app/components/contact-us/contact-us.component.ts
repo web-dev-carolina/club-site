c8e400f3849bab75abd4eaff3bb3b6bffb0b1bad
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-04 13:16:20 -0500

    Add state management for contact us form

--- client/my-app/src/app/components/contact-us/contact-us.component.ts
+++ client/my-app/src/app/components/contact-us/contact-us.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: 'contact-us.component.html',
   styleUrls: ['contact-us.component.css'],
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements AfterContentInit {
   state = '';
   constructor(){
     this.state = 'potenital member';
+  }
+  ngAfterContentInit(): void{
+    window.onload = () => {
+      document.getElementById('potentialMemberLink').onclick = () => {
+        this.state = 'potenital member';
+        console.log(this.state);
+      };
+      document.getElementById('potentialClientLink').onclick = () => {
+        this.state = 'potential client';
+        console.log(this.state);
+      };
+      document.getElementById('generalLink').onclick = () => {
+        this.state = 'general';
+        console.log(this.state);
+      };
+    }
   }
 }
