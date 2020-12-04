eba20110426b622eb039c4c1bb389f889bba6c95
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-04 13:56:59 -0500

    Add switching icon color on click for contact form

--- client/my-app/src/app/components/contact-us/contact-us.component.ts
+++ client/my-app/src/app/components/contact-us/contact-us.component.ts
@@ -14,14 +14,23 @@ export class ContactUsComponent implements AfterContentInit {
     window.onload = () => {
       document.getElementById('potentialMemberLink').onclick = () => {
         this.state = 'potenital member';
+        document.getElementById('memberImg').setAttribute('src', 'assets/images/members.png');
+        document.getElementById('clientImg').setAttribute('src', 'assets/images/Client.png');
+        document.getElementById('generalImg').setAttribute('src', 'assets/images/questions.png');
         console.log(this.state);
       };
       document.getElementById('potentialClientLink').onclick = () => {
         this.state = 'potential client';
+        document.getElementById('clientImg').setAttribute('src', 'assets/images/ClickedClient.png');
+        document.getElementById('memberImg').setAttribute('src', 'assets/images/UnclickedMember.png');
+        document.getElementById('generalImg').setAttribute('src', 'assets/images/questions.png');
         console.log(this.state);
       };
       document.getElementById('generalLink').onclick = () => {
         this.state = 'general';
+        document.getElementById('generalImg').setAttribute('src', 'assets/images/ClickedGeneral.png');
+        document.getElementById('memberImg').setAttribute('src', 'assets/images/UnclickedMember.png');
+        document.getElementById('clientImg').setAttribute('src', 'assets/images/Client.png');
         console.log(this.state);
       };
     };
