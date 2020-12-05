f72dec7b133712b03beaa888bd30fa2db0b1f450
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-05 10:28:41 -0500

    FIx issue with declaring objects in html file

--- client/my-app/src/app/components/testimonial/testimonial.module.ts
+++ client/my-app/src/app/components/testimonial/testimonial.module.ts
@@ -1,12 +1,13 @@
 // Angular Imports
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 // This Module's Components
 import { TestimonialComponent } from './testimonial.component';
 
 @NgModule({
     imports: [
-
+        CommonModule
     ],
     declarations: [
         TestimonialComponent,
