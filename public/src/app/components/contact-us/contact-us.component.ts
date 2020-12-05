40e850cec0b5a2691d3c25d9346724c8767456d5
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-05 18:37:44 -0500

    Member submit errors added if blank input

--- public/src/app/components/contact-us/contact-us.component.ts
+++ public/src/app/components/contact-us/contact-us.component.ts
@@ -22,27 +22,41 @@ export class ContactUsComponent implements AfterContentInit {
         const lastName = (document.getElementById('inputLastName') as HTMLInputElement).value;
         const email = (document.getElementById('inputEmail') as HTMLInputElement).value;
         const message = (document.getElementById('inputMessage') as HTMLInputElement).value;
-        const resp = this.memberFormService.postForm(firstName, lastName, email, message);
-        resp.then(data => {if (data !== false){
-          if (!document.getElementById('divSuccess')){
-            const div = document.createElement('div');
-            const del = document.createElement('button');
-            const span = document.createElement('span');
-            div.setAttribute('class', 'alert alert-success alert-dismissible fade show');
-            div.setAttribute('id', 'divSuccess');
-            div.innerHTML = 'You\'ve contacted us. We\'ll get back to you as soon as possible';
-            del.setAttribute('type', 'button');
-            del.setAttribute('class', 'close');
-            del.setAttribute('data-dismiss', 'alert');
-            del.setAttribute('aria-label', 'Close');
-            del.setAttribute('id', 'delNoti');
-            span.setAttribute('aria-hidden', 'true');
-            span.innerHTML = '&times;';
-            del.appendChild(span);
-            div.appendChild(del);
-            document.getElementById('memberForm').appendChild(div);
-          }
-        }});
+        if (firstName.length === 0){
+          (document.getElementById('inputFirstName') as HTMLInputElement).setAttribute('class', 'form-control is-invalid');
+        }
+        if (lastName.length === 0) {
+          (document.getElementById('inputLastName') as HTMLInputElement).setAttribute('class', 'form-control is-invalid');
+        }
+        if (email.length === 0) {
+          (document.getElementById('inputEmail') as HTMLInputElement).setAttribute('class', 'form-control is-invalid');
+        }
+        if (message.length === 0) {
+          (document.getElementById('inputMessage') as HTMLInputElement).setAttribute('class', 'form-control is-invalid');
+        }
+        if (firstName.length !== 0 || lastName.length !== 0 || email.length !== 0 || message.length !== 0) {
+          const resp = this.memberFormService.postForm(firstName, lastName, email, message);
+          resp.then(data => {if (data !== false){
+            if (!document.getElementById('divSuccess')){
+              const div = document.createElement('div');
+              const del = document.createElement('button');
+              const span = document.createElement('span');
+              div.setAttribute('class', 'alert alert-success alert-dismissible fade show');
+              div.setAttribute('id', 'divSuccess');
+              div.innerHTML = 'You\'ve contacted us. We\'ll get back to you as soon as possible';
+              del.setAttribute('type', 'button');
+              del.setAttribute('class', 'close');
+              del.setAttribute('data-dismiss', 'alert');
+              del.setAttribute('aria-label', 'Close');
+              del.setAttribute('id', 'delNoti');
+              span.setAttribute('aria-hidden', 'true');
+              span.innerHTML = '&times;';
+              del.appendChild(span);
+              div.appendChild(del);
+              document.getElementById('memberForm').appendChild(div);
+            }
+          }});
+        }
       } else if (id === 'clientSubmit') {
         const name = (document.getElementById('inputName') as HTMLInputElement).value;
         const business = (document.getElementById('inputLastName') as HTMLInputElement).value;
