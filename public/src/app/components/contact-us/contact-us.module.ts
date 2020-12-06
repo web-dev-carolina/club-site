46214b173930ec7595f40d69f19e8d25333a6351
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-05 19:23:46 -0500

    Edit contact us module to remove angular problem

--- public/src/app/components/contact-us/contact-us.module.ts
+++ public/src/app/components/contact-us/contact-us.module.ts
@@ -1,12 +1,13 @@
 // Angular Imports
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 // This Module's Components
 import { ContactUsComponent } from './contact-us.component';
 
 @NgModule({
     imports: [
-
+        CommonModule
     ],
     declarations: [
         ContactUsComponent,
