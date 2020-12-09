244daa7757593f544dd5879f70d7fc6e40af1dce
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-09 18:13:13 -0500

    Add forms to inquiries component

---
+++ public/src/GeneralForm.ts
@@ -0,0 +1,6 @@
+export class GeneralForm {
+    name: string;
+    affiliation: string;
+    email: string;
+    message: string;
+}
--- public/src/app/components/logged-in/admin/inquiries/inquiries.component.ts
+++ public/src/app/components/logged-in/admin/inquiries/inquiries.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { MemberFormService } from '../../../../services/memberForm.service';
 import { ClientFormService } from '../../../../services/clientForm.service';
 import { GeneralFormService } from '../../../../services/generalForm.service';
+import { MemberForm } from '../../../../../MemberForm';
+import { ClientForm } from '../../../../../ClientForm';
+import { GeneralForm } from '../../../../../GeneralForm';
+
 
 @Component({
     selector: 'app-inquiries',
@@ -9,13 +13,21 @@ import { GeneralFormService } from '../../../../services/generalForm.service';
     styleUrls: ['inquiries.component.css']
 })
 export class InquiriesComponent {
-    memberForms: object[];
+    memberForms: MemberForm[];
+    clientForms: ClientForm[];
+    generalForms: GeneralForm[];
     constructor(private memberFormService: MemberFormService,
                 private clientFormService: ClientFormService,
                 private generalFormService: GeneralFormService){
         this.memberForms = [];
-        this.memberFormService.getForms().subscribe((memberForm: any[]) => {
-            this.memberForms.push(memberForm);
+        this.memberFormService.getForms().subscribe((memberForms: any[]) => {
+            this.memberForms = memberForms;
+        });
+        this.clientFormService.getForms().subscribe((clientForms: any[]) => {
+            this.clientForms = clientForms;
+        });
+        this.generalFormService.getForms().subscribe((generalForms: any[]) => {
+            this.generalForms = generalForms;
         });
     }
 }
