4133ee50afe68a9fb32c96ad69e7ec8434d2ec2f
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-09 15:55:38 -0500

    Remove old file

--- public/src/app/components/logged-in/logged-in.component.ts
+++ public/src/app/components/logged-in/logged-in.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-logged-in',
     templateUrl: 'logged-in.component.html',
-    styleUrls: ['logged-in.component.css']
 })
 export class LoggedInComponent {
 
