3908803c33eda9087ed121015aa07590f3cf42b9
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-09 17:21:49 -0500

    Add testimonial to admin create

--- public/src/app/components/logged-in/admin/create/create.component.ts
+++ public/src/app/components/logged-in/admin/create/create.component.ts
@@ -3,6 +3,8 @@ import { UpcomingEventsService } from '../../../../services/upcomingEvents.servi
 import { UpcomingEvent } from '../../../../../UpcomingEvent';
 import { AnnouncementService } from '../../../../services/announcements.service';
 import { Announcement } from '../../../../../Announcement';
+import { TestimonialService } from '../../../../services/testimonials.service';
+import { Testimonial } from '../../../../../Testimonial';
 
 @Component({
     selector: 'app-create',
@@ -12,13 +14,18 @@ import { Announcement } from '../../../../../Announcement';
 export class CreateComponent {
     upcomingEvents: UpcomingEvent[];
     announcements: Announcement[];
+    testimonials: Testimonial[];
 
-    constructor(private upcomingEventService: UpcomingEventsService, private annoucementService: AnnouncementService) {
+    constructor(private upcomingEventService: UpcomingEventsService, private annoucementService: AnnouncementService,
+                private testimonialService: TestimonialService) {
       this.upcomingEventService.getUpcomingEvents().subscribe((upcomingEvents: any[]) => {
         this.upcomingEvents = upcomingEvents;
       });
       this.annoucementService.getAnnouncements().subscribe((announcements: any[]) => {
         this.announcements = announcements;
+      });
+      this.testimonialService.getTestimonials().subscribe((testimonials: any[]) => {
+        this.testimonials = testimonials;
       });
     }
 }
