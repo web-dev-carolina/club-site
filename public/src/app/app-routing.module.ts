d83eced44483c43a9144526766a6d490815d7e36
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-08 20:59:39 -0500

    Nested routing

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/landing/login/login.component';
 import { SinglePComponent } from './components/landing/single-page/single-p.component';
 
 const routes: Routes = [
   {path: '**', redirectTo: 'landing'},
-  {path: 'landing', component: LandingComponent, children: [{path: '', component: SinglePComponent}]}
+  {path: 'landing', component: LandingComponent, children: [{path: '', component: SinglePComponent}, 
+    {path: 'login', component: LoginComponent}]}
 ];
 
 @NgModule({
