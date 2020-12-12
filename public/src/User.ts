18702c743918c5b791e54600f3685e1a8ef64687
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-12 10:14:06 -0500

    Add user constructor

--- public/src/User.ts
+++ public/src/User.ts
@@ -2,4 +2,9 @@ export class User {
     username: string;
     password: string;
     type: string;
+    constructor(username, password, type){
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
 }
