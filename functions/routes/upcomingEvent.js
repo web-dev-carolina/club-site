4238e6b2cd0fd6e64a098fb7769b183d4e456432
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2020-12-11 23:32:26 -0500

    update endpoint

--- functions/routes/upcomingEvent.js
+++ functions/routes/upcomingEvent.js
@@ -42,10 +42,28 @@ router.delete('/upcomingEvents/:id', async function (req, res, next) {
     const client = await getClient();
     const db = client.db("club-site");
     const collection = db.collection("upcomingEvents");
-    
-    collection.deleteOne({title: req.params.id});
+
+    collection.deleteOne({ title: req.params.id });
   } catch (err) {
     res.status(500).send('Something broke!');
+  }
+});
+
+router.put("/upcomingEvents/:id", async function (req, res, next) {
+  try {
+    const client = await getClient();
+    const db = client.db("club-site");
+    const collection = db.collection("upcomingEvents");
+
+    const item = req.body;
+    const event = await collection.findById(req.params._id);
+    event.title = item.title;
+    event.body = item.body;
+    event.day = item.day;
+    event.month = item.month;
+    event.save();
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
--- public/src/app/services/upcomingEvents.service.ts
+++ public/src/app/services/upcomingEvents.service.ts
@@ -8,20 +8,25 @@ export class UpcomingEventsService {
   }
 
   // tslint:disable-next-line:typedef
-  getUpcomingEvents(){
+  getUpcomingEvents() {
      return this.http.get('https://us-central1-home-c6ab1.cloudfunctions.net/app/api/upcomingEvents');
   }
 
   // tslint:disable-next-line:typedef
-  newUpcomingEvent(t, b, d, m){
+  newUpcomingEvent(t, b, d, m) {
     return this.http.post('https://us-central1-home-c6ab1.cloudfunctions.net/app/api/upcomingEvents',
-    { title: t, body: b, day: d, month: m})
-    .toPromise();
+    { title: t, body: b, day: d, month: m}).toPromise();
   }
 
   // tslint:disable-next-line:typedef
   deleteEvent(t) {
     return this.http.delete('https://us-central1-home-c6ab1.cloudfunctions.net/app/api/upcomingEvents/' + t)
     .toPromise();
+  }
+
+  // tslint:disable-next-line:typedef
+  updateEvent(title, body, day, month) {
+    return this.http.put('https://us-central1-home-c6ab1.cloudfunctions.net/app/api/upcomingEvents/' + title,
+    { title, body, day, month}).toPromise();
   }
 }
