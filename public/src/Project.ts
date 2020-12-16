353285bc2914e19fc053a501986153a730d793de
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-16 14:52:32 -0500

    Update project object and display

--- public/src/Project.ts
+++ public/src/Project.ts
@@ -1,10 +1,14 @@
 export class Project {
     name: string;
+    client: string;
     desc: string;
     meet: string;
-    constructor(name, desc, meet){
+    github: string;
+    constructor(name, client, desc, meet, github){
         this.name = name;
+        this.client = client;
         this.desc = desc;
         this.meet = meet;
+        this.github = github;
     }
 }
