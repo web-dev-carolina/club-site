140e9d3801207a50a4d8b9fad96d5833969d0671
cnell7 <46611006+cnell7@users.noreply.github.com>
2020-12-18 14:21:54 -0500

    Re format

--- public/src/app/components/logged-in/admin/inquiries/inquiries.component.ts
+++ public/src/app/components/logged-in/admin/inquiries/inquiries.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MemberFormService } from '../../../../services/memberForm.service';
 import { ClientFormService } from '../../../../services/clientForm.service';
 import { GeneralFormService } from '../../../../services/generalForm.service';
@@ -12,7 +12,7 @@ import { GeneralForm } from '../../../../../GeneralForm';
     templateUrl: 'inquiries.component.html',
     styleUrls: ['inquiries.component.css']
 })
-export class InquiriesComponent {
+export class InquiriesComponent implements OnInit {
     memberForms: MemberForm[];
     clientForms: ClientForm[];
     generalForms: GeneralForm[];
@@ -31,21 +31,26 @@ export class InquiriesComponent {
         this.generalFormService.getForms().subscribe((generalForms: any[]) => {
             this.generalForms = generalForms;
         });
-        window.addEventListener('click', (e) => {
-            const element = (e.target as HTMLElement);
-            const clas = element.getAttribute('class');
-            const id = element.id;
-            if (clas.includes('formToggle')){
-              if (id === 'memberForm'){
-                this.display = 'memberForm';
-              } else if (id === 'clientForm'){
-                this.display = 'clientForm';
-              } else if (id === 'generalForm'){
-                this.display = 'generalForm';
-              } else if (id === 'all'){
-                this.display = 'all';
-              }
-            }
-          });
+    }
+    ngOnInit(): void {
+      window.addEventListener('click', (e) => {
+        const element = (e.target as HTMLElement);
+        const clas = element.getAttribute('class');
+        const id = element.id;
+        if (clas === null || clas === undefined){
+          return;
+        }
+        if (clas.includes('formToggle')){
+          if (id === 'memberForm'){
+            this.display = 'memberForm';
+          } else if (id === 'clientForm'){
+            this.display = 'clientForm';
+          } else if (id === 'generalForm'){
+            this.display = 'generalForm';
+          } else if (id === 'all'){
+            this.display = 'all';
+          }
+        }
+      });
     }
 }
